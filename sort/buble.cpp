93c12b4aec7c1ba387eeaa1113dfbf703a5cea1a
Софія <sofiia@Sofias-MacBook-Air.local>
2026-02-14 22:29:20 +0200

    алгоритм сортування

---
+++ sort/buble.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+// Функція для виведення масиву
+void printArray(int arr[], int size) 
+{
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+//
+void coctailSort(int arr[], int size) {
+    bool swapped = true;
+    int start = 0;
+    int end = size - 1;
+
+    while (swapped) {
+        swapped = false;
+    }
+}
+
+int main() {
+
+}
