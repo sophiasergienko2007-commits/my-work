f7dcc9019cd79d625ccf01ca0f7c86628e5840b6
Софія <sofiia@Sofias-MacBook-Air.local>
2026-02-15 16:38:15 +0200

    коктельне сортування

--- sort/buble.cpp
+++ sort/buble.cpp
@@ -3,25 +3,57 @@
 using namespace std;
 
 // Функція для виведення масиву
-void printArray(int arr[], int size) 
-{
+void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-//
-void coctailSort(int arr[], int size) {
+//функція Coctail Sort
+void cocktailSort(int arr[], int size) {
     bool swapped = true;
     int start = 0;
     int end = size - 1;
 
     while (swapped) {
         swapped = false;
+
+// прохід зліва - направо
+for (int i = start; i < end; ++i) {
+    if (arr[i] > arr[i + 1]) {
+        swap(arr[i], arr[i + 1]);
+        swapped = true;
+    }
+}
+    if (!swapped) break; 
+
+        swapped = false; 
+        end--; // останнє число не чіпаємо
+
+// прохід справа наліво 
+        for (int i = end - 1; i >= start; --i) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i]; //метод 3 склянок 
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        start++; 
     }
 }
 
 int main() {
+    int numbers[] = {5, 25, 7, 2, 10};
+    int size = 5;
+
+    cout << "Starting sort: ";
+    printArray(numbers, size);
+    
+    cocktailSort(numbers, size);
+    
+    cout << "Sorted with Cocktail Sort: ";
+    printArray(numbers, size);
 
 }
